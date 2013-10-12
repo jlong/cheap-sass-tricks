459c36af40a7693b944c19551d43950f5e323d3c
John W. Long <john@uservoice.com>
2013-10-11 23:53:08 -0400

    add keyboard navigation

---
+++ public/javascripts/navigation.js
@@ -0,0 +1,26 @@
+var pages = [
+  '/pbj',
+  '/lorenz',
+  '/spinner',
+  '/dots',
+  '/plus',
+  '/atebits',
+  '/ball',
+  '/radar',
+  '/matrix'
+];
+
+var path = location.pathname;
+
+KeyboardJS.on('left', function() {
+  var n = pages.indexOf(path);
+  n = n - 1;
+  if (n < 0) { n = pages.length - 1 }
+  window.location.pathname = pages[n];
+});
+KeyboardJS.on('right', function() {
+  var n = pages.indexOf(path);
+  n = n + 1;
+  if (n >= pages.length) { n = 0 }
+  window.location.pathname = pages[n];
+});
